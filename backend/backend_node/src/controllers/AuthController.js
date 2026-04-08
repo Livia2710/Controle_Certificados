fe0535f5f463c59ca8e188e4d38212c079d23019
Gabb.Flor <gabriel.florindo.souza46@gmail.com>
2026-04-08 19:31:49 -0300

    feat
    implementando validador do Zod nas rotas de Auth

--- backend/backend_node/src/controllers/AuthController.js
+++ backend/backend_node/src/controllers/AuthController.js
@@ -1,16 +1,14 @@
+import { BodyLoginDTO } from "../DTOs/BodyLoginDTO.js";
+import { BodyRegistroDTO } from "../DTOs/BodyRegistroDTO.js";
 import AuthService from "../services/AuthService.js";
 import HttpException from "../utils/HttpException.js";
+import { ZodError } from "zod";
 
 class AuthController {
     async login(req, res) {
         try {
             // implementar filtro com zod, por enquanto vou deixar na gambiarra msm
-            const { email, password } = req.body;
-
-            const userData = ({
-                email, 
-                password
-            })
+            const userData = BodyLoginDTO.parse(req.body)
 
             const response = await AuthService.login(userData);
 
@@ -19,20 +17,18 @@ class AuthController {
             if (err instanceof HttpException) {
                 return res.status(err.status).json({ erro: err.message });
             }
+            if (err instanceof ZodError) {
+                return res.status(400).json({
+                    error: err.issues[0].message
+                });
+            }
             return res.status(500).json({ erro: `An error occurred while trying to log in: ${err.message}` });
         }
     }
 
     async register(req, res) {
         try {
-            // implementar filtro com zod, por enquanto vou deixar na gambiarra msm
-            const { username, email, password } = req.body;
-
-            const userData = ({
-                username,
-                email, 
-                password
-            })
+            const userData = BodyRegistroDTO.parse(req.body)
 
             await AuthService.register(userData);
 
@@ -40,6 +36,11 @@ class AuthController {
         } catch(err) {
             if (err instanceof HttpException) {
                 return res.status(err.status).json({ erro: err.message });
+            }
+            if (err instanceof ZodError) {
+                return res.status(400).json({
+                    error: err.issues[0].message
+                });
             }
             return res.status(500).json({ erro: `An error occurred while trying to register the user: ${err.message}` });
         }
